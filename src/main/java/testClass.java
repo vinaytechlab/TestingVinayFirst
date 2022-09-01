33770cfaba9d6a266e4267a48a4f07fb3f8602ec
vinaytechlab <112630098+vinaytechlab@users.noreply.github.com>
2022-09-01 18:41:40 +0530

    Update testClass.java

--- src/main/java/testClass.java
+++ src/main/java/testClass.java
@@ -3,7 +3,7 @@ public class testClass {
 
 	public static void main(String[] args) {
 
-		System.out.println("This is my first Interview");
+		System.out.println("This is my first In terview");
 	}
 
 }
